ad71201d17943ef99f6cde9979ac877e256d1adc
2lu3 <hi2lu3@gmail.com>
2019-11-06 20:52:52 +0900

    get_train/valid/test関数でoptionを指定できるようにした

--- mymodules/dataset.py
+++ mymodules/dataset.py
@@ -11,30 +11,26 @@ class Dataset():
                     [[['tokyo'], [2018], [5,9]]]
                     ]
         self.option = option
-        train_data = loaddata.createDataSet(
-                self.heatstroke_dic,
-                self.weather_dic,
-                self.prefecture_pd,
-                self.option[0],
-                verbose=0)
-        valid_data = loaddata.createDataSet(
-                self.heatstroke_dic,
-                self.weather_dic,
-                self.prefecture_pd,
-                self.option[1],
-                verbose=0)
+        self.datasets = [None, None, None]
 
-        test_dat = loaddata.createDataSet(
-                self.heatstroke_dic,
-                self.weather_dic,
-                self.prefecture_pd,
-                self.option[2],
-                verbose=0)
+    def get_dataset(self, data_type_number, option = None):
+        if type(option) is not None:
+            self.datasets[data_type_number] = None
+        else:
+            option = self.option[data_type_number]
+        if type(self.datasets[data_type_number] is None):
+            self.datasets[data_type_number] = loaddata.createDataSet(
+                    self.heatstroke_dic,
+                    self.weather_dic,
+                    self.prefecture_pd,
+                    option,
+                    verbose=0)
+        return self.datasets[data_type_number].copy()
 
 
-    def get_train(self):
-        return train_data.copy()
-    def get_valid(self):
-        return valid_data.copy()
-    def get_test(self):
-        return test_data.copy()
+    def get_train(self, option = None):
+        return self.get_dataset(0, option)
+    def get_valid(self, option=None):
+        return self.get_dataset(1, option)
+    def get_test(self, option = None)
+        return self.get_dataset(2, option)
