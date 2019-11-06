d44b127a45ea86874c8dca3cf10daa8b0b3a996f
2lu3 <hi2lu3@gmail.com>
2019-11-06 21:35:43 +0900

    バグを修正

--- mymodules/dataset.py
+++ mymodules/dataset.py
@@ -2,8 +2,8 @@ import loaddata
 
 class Dataset():
     def __init__(self, option=None, root_dir='ai-watch-data/'):
-        pddics = loaddata.loadPdDic(root_dir)
-        self.heatstroke_dic, self.weather_dic, self.prefecture_pd = pddics
+        self.heatstroke_dic, self.weather_dic, self.prefecture_pd = loaddata.loadPdDic(root_dir)
+
         if option is None:
             option = [
                     [[['tokyo'], [i for i in range(2008, 2017)], [5, 9]]],
