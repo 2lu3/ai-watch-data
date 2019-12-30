59f310edb662d5d32f64467df04cd5e71f9aa833
2lu3 <hi2lu3@gmail.com>
2019-12-30 17:11:32 +0900

    updated

--- code/feature.py
+++ code/feature.py
@@ -15,7 +15,7 @@ class Feature():
   def get_dataset(self):
     return self.data.copy()
 
-  def register_feature(self, feature, feature_name):
+  def register_feature(self, feature, feature_name, feature_describe):
     Util.dump_feature(feature, feature_name)
 
   def standarlization(self):
@@ -23,4 +23,5 @@ class Feature():
       if self.data[name][0] is int or self.data[name][0] is float:
         self.data[name] = ((self.data[name] - self.data[name].mean())
             / self.data[name].std(ddof=0))
+
 
--- code/model_lgb.py
+++ code/model_lgb.py
@@ -50,12 +50,121 @@ class ModelLGB(Model):
     valid_prediction = self.model.predict(te_X, num_iteration=self.model.best_iteration)
     return valid_prediction, fold_importance
 
-  def save_model(self):
-    here = os.path.dirname(__file__)
-    model_path = os.path.join('here' + '../model/model', f'{self.run_hold_name}.lgbm')
+  def save_model(self, path):
+    here_path = os.path.dirname(__file__)
+    model_path = os.path.join(here_path + '../model/', path, f'/{self.run_hold_name}.lgbm')
     Util.dump(self.model, model_path)
 
-  def load_model(self):
-    model_path = os.path.join('../model/model', f'{self.run_hold_name}.lgbm')
+  def load_model(self, path):
+    model_path = os.path.join('../model/', path, f'{self.run_hold_name}.lgbm')
     self.model = Util.load(model_path)
+
+  @classmethod
+  def calc_feature_importance(self, di_name, run_name, features):
+    """ feature importanceの計算
+    """
+    val_split = model_array[0].feature_importance(importance_type='splut')
+    val_grain = model_array[0].feature_importance(importance_type='gain')
+    val_split = pd.Series(val_split)
+    val_gain = pd.Series(val_gain)
+
+    for model in model_array[1:]:
+      s = pd.Series(model.feature_importance(importance_type='split'))
+      val_split = pd.concat([val_split, s], axis=1)
+      s = pd.Series(model.feature_importance(importance_type='gain'))
+      val_gain = pd.concat([val_gain, s], axis=1)
+
+    # ------------
+    # splitの計算
+    # ------------
+    # 各foldの平均を算出
+    val_mean = val_split.mean(axis=1).values
+    importance_df_medan = pd.DataFrame(val_mean, index=features, columns=['importance']).sort_values('importance')
+
+    # 各foldの標準偏差を算出
+    val_std = val_split.std(axis=1).values
+    importance_df_std = pd.DataFrame(val_std, index=features, columns=['importance']).sort_values('importance')
+
+    # マージ
+    df = pd.merge(importance_df_mean, importance_df_std, left_index=True, right_index=True ,suffixes=['_mean', '_std'])
+
+    df['coef_of_var'] = df['importance_std'] / df['importance_mean']
+    df['coef_of_var'] = df['coef_of_var'].fillna(0)
+    df = df.sort_values('importance_mean', ascending=True)
+
+    # 出力
+    fig, ax1 = plt.subplots(figsize = (10, 10))
+    plt.tick_params(labelsize=12) # 図のラベルのfontサイズ
+    plt.tight_layout()
+
+    # 棒グラフを出力
+    ax1.set_title('feature importance split')
+    ax1.set_xlabel('feature importance mean & std')
+    ax1.barh(df.index, df['importance_mean'], label='importance_mean',  align="center", alpha=0.6)
+    ax1.barh(df.index, df['importance_std'], label='importance_std',  align="center", alpha=0.6)
+
+    # 折れ線グラフを出力
+    ax2 = ax1.twiny()
+    ax2.plot(df['coef_of_var'], df.index, linewidth=1, color="crimson", marker="o", markersize=8, label='coef_of_var')
+    ax2.set_xlabel('Coefficient of variation')
+
+    #凡例を表示（グラフ左上、ax2をax1のやや下に持っていく）
+    ax1.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0.5, fontsize=12)
+    ax2.legend(bbox_to_anchor=(1, 0.93), loc='upper right', borderaxespad=0.5, fontsize=12)
+
+    #グリッド表示(ax1のみ)
+    ax1.grid(True)
+    ax2.grid(False)
+
+    plt.savefig(dir_name + run_name + '_fi_split.png', dpi=300, bbox_inches="tight")
+    plt.close()
+
+
+    # -----------
+    # gainの計算
+    # -----------
+    # 各foldの平均を算出
+    val_mean = val_gain.mean(axis=1)
+    val_mean = val_mean.values
+    importance_df_mean = pd.DataFrame(val_mean, index=features, columns=['importance']).sort_values('importance')
+
+    # 各foldの標準偏差を算出
+    val_std = val_gain.std(axis=1)
+    val_std = val_std.values
+    importance_df_std = pd.DataFrame(val_std, index=features, columns=['importance']).sort_values('importance')
+
+    # マージ
+    df = pd.merge(importance_df_mean, importance_df_std, left_index=True, right_index=True ,suffixes=['_mean', '_std'])
+
+    # 変動係数を算出
+    df['coef_of_var'] = df['importance_std'] / df['importance_mean']
+    df['coef_of_var'] = df['coef_of_var'].fillna(0)
+    df = df.sort_values('importance_mean', ascending=True)
+
+    # 出力
+    fig, ax1 = plt.subplots(figsize = (10, 10))
+    plt.tick_params(labelsize=12) # 図のラベルのfontサイズ
+    plt.tight_layout()
+
+    # 棒グラフを出力
+    ax1.set_title('feature importance gain')
+    ax1.set_xlabel('feature importance mean & std')
+    ax1.barh(df.index, df['importance_mean'], label='importance_mean',  align="center", alpha=0.6)
+    ax1.barh(df.index, df['importance_std'], label='importance_std',  align="center", alpha=0.6)
+
+    # 折れ線グラフを出力
+    ax2 = ax1.twiny()
+    ax2.plot(df['coef_of_var'], df.index, linewidth=1, color="crimson", marker="o", markersize=8, label='coef_of_var')
+    ax2.set_xlabel('Coefficient of variation')
+
+    # 凡例を表示（グラフ左上、ax2をax1のやや下に持っていく）
+    ax1.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0.5, fontsize=12)
+    ax2.legend(bbox_to_anchor=(1, 0.93), loc='upper right', borderaxespad=0.5, fontsize=12)
+
+    # グリッド表示(ax1のみ)
+    ax1.grid(True)
+    ax2.grid(False)
+
+    plt.savefig(dir_name + run_name + '_fi_gain.png', dpi=300, bbox_inches="tight")
+    plt.close()
 
